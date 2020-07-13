6c76c29f9f8638c362b7b03b314d562b4469b55d
Jeroen Langelaan <jeroen@wooting.nl>
2020-07-13 16:32:40 +0200

    Add keybinding handlers to React Piano

---
+++ src/components/Piano.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+//@ts-ignore
+import { Piano } from "react-piano";
+import "react-piano/dist/styles.css";
+import { MidiEntry } from "../App";
+import { HIDCodes } from "../HidCodes";
+
+interface Props {
+  midiData: MidiEntry[];
+  changeMidiMap: (midiNumber: number) => void;
+}
+
+// We need to be careful with the rendering of this component. Any rerenders reset animations (like click) in the piano display
+export const PianoDisplay = React.memo((props: Props) => {
+  const keyboardShortcuts = props.midiData.map((data) => {
+    return { key: HIDCodes[data.key], midiNumber: data.note };
+  });
+
+  return (
+    <Piano
+      playNote={(midiNumber: number) => props.changeMidiMap(midiNumber)}
+      stopNote={() => null}
+      noteRange={{ first: 21, last: 108 }}
+      activeNotes={props.midiData
+        .filter((data) => {
+          return data.value >= 0.1;
+        })
+        .map((data) => data.note)}
+      keyboardShortcuts={keyboardShortcuts}
+    />
+  );
+});
