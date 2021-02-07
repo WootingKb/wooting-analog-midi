763dce6cdde035768717f485fdc8fd2fe4100189
simon-wh <simon@wooting.io>
2021-02-07 17:20:59 +0000

    Proper window title

--- src/main/main.ts
+++ src/main/main.ts
@@ -7,6 +7,7 @@ function createWindow() {
   mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    title: "Wooting Analog Midi",
     webPreferences: {
       nodeIntegration: true,
     },
